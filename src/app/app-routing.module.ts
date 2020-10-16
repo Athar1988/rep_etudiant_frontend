fa17652579141362bcf34e9a3f696547f4f953a2
Athar1988 <hakim.athar.88@gmail.com>
2020-10-16 14:20:48 +0100

    partie frontend tt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EtudiantComponent} from './etudiant/etudiant.component';
+import {InscriptionComponent} from './inscription/inscription.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'etudiant', component: EtudiantComponent},
+  {path: 'inscription', component: InscriptionComponent}
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EtudiantComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
